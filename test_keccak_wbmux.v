dbb635d589b7185a42826a6a01e961d7ed8ea871
sen1113 <doradoradoradorax@yahoo.co.jp>
2015-12-07 18:01:59 +0900

    modify for keccak

--- test_keccak_wbmux.v
+++ test_keccak_wbmux.v
@@ -6,9 +6,9 @@ This module is testbench for or1200_wbmux.v
 
 `timescale 1ns / 1ps
 `define P 20
+`define width 32
 `include "or1200_defines.v"
 
-parameter width = `OR1200_OPERAND_WIDTH;
 
 module test_keccak_wbmux;
 
@@ -18,21 +18,21 @@ module test_keccak_wbmux;
 
    reg wb_freeze;
    reg [`OR1200_RFWBOP_WIDTH-1:0] rfwb_op;
-   reg [width-1:0] 		  muxin_a;//alu_dataout
-   reg [width-1:0] 		  muxin_b;//lsu_dataout
-   reg [width-1:0] 		  muxin_c;//sprs_dataout
-   reg [width-1:0] 		  muxin_d;//ex_pc
-   reg [width-1:0] 		  muxin_e;//fpu_dataout
+   reg [`width-1:0] 		  muxin_a;//alu_dataout
+   reg [`width-1:0] 		  muxin_b;//lsu_dataout
+   reg [`width-1:0] 		  muxin_c;//sprs_dataout
+   reg [`width-1:0] 		  muxin_d;//ex_pc
+   reg [`width-1:0] 		  muxin_e;//fpu_dataout
 
    reg [31:0] 			  muxin_keccak;//keccak_dataout
    // Outputs
-   wire [width-1:0] 		  muxout;//rf_dataw
-   wire [width-1:0] 		  muxreg;//wb_forw
+   wire [`width-1:0] 		  muxout;//rf_dataw
+   wire [`width-1:0] 		  muxreg;//wb_forw
    wire 			  muxreg_valid;//wbforw_valid
 
 
    // Instantiate the Unit Under Test (UUT)
-module or1200_wbmux(
+or1200_wbmux or1200_wbmux(
 		    // Clock and reset
 		    clk, rst,
 
@@ -48,12 +48,12 @@ module or1200_wbmux(
       rst = 0;
       wb_freeze = 0;
       rfwb_op = 0;
-      muxin_a = 32'h1000_0000_0000_0000;
-      muxin_b = 32'h0100_0000_0000_0000;
-      muxin_c = 32'h0010_0000_0000_0000;
-      muxin_d = 32'h0001_0000_0000_0000;
-      muxin_e = 32'h1001_0000_0000_0000;
-      muxin_keccak = 32'h1111_1111_1111_1111;
+      muxin_a = 32'h1000_0000;
+      muxin_b = 32'h0100_0000;
+      muxin_c = 32'h0010_0000;
+      muxin_d = 32'h0001_0000;
+      muxin_e = 32'h1001_0000;
+      muxin_keccak = 32'h1111_1111;
       // Wait 100 ns for global reset to finish
       #100;
 
@@ -65,22 +65,24 @@ module or1200_wbmux(
 
       //test muxin_keccak
       #10;
-      rfwb_op = 0001;//ALU
+      rfwb_op = 4'b0001;//ALU
       #10;
-      rfwb_op = 0011;//LSU
+      rfwb_op = 4'b0011;//LSU
       #10;
-      rfwb_op = 0101;//SPRS
+      rfwb_op = 4'b0101;//SPRS
       #10;
-      rfwb_op = 0111;//LR
+      rfwb_op = 4'b0111;//LR
       #10;
-      rfwb_op = 1001;//FPU
+      rfwb_op = 4'b1001;//FPU
       #100;
-      rfwb_op = 1111;//KECCAK
+      rfwb_op = 4'b1111;//KECCAK
       #100;
-      rfwb_op = 1110;//if rfwb_op[0] = 0,wb is not enabled.
+      rfwb_op = 4'b1110;//if rfwb_op[0] = 0,wb is not enabled.
 
       #100;
       wb_freeze = 1;
+      #10;
+      rfwb_op = 4'b0001;
    end
 
 
