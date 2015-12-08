3471001654375c0847019872732249c1632ff55f
sen1113 <doradoradoradorax@yahoo.co.jp>
2015-12-09 02:40:31 +0900

    modifygit add -A

--- keccak_ctrl.v
+++ keccak_ctrl.v
@@ -17,12 +17,12 @@ module keccak_ctrl(clk,reset,
    input 	 keccak_en;//from alu
    input 	 devide_en;//from keccak(=out_ready)
 
-   output 	 byte_num;//to keccak
+   output [1:0] byte_num;//to keccak
    output 	 in_ready;//to keccak
    output [31:0] hash_out32;//to wbmux
 
 
-   assign byte_num = 2'b11; //Fix byte_num = 3 temporarily
+   assign byte_num = 2'b00; //Fix byte_num = 0 temporarily,because input is 32bit
    assign in_ready = keccak_en && (!devide_en);
 
    keccak_devide keccak_devide(
