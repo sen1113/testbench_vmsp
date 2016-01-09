68e62d766aae5b7dd1cd27528967a10372761995
sen1113 <doradoradoradorax@yahoo.co.jp>
2016-01-10 01:34:17 +0900

    r

--- keccak_ctrl.v
+++ keccak_ctrl.v
@@ -17,17 +17,20 @@
    output [1:0]  byte_num;//to keccak
    output 	 in_ready;//to keccak
    output [31:0] hash_out32;//to wbmux
-
+wire[31:0]   devided32;
 
    assign byte_num = 2'b00; //Fix byte_num = 0 temporarily,because input is 32bit
    assign in_ready = keccak_en && (!out_ready);
    assign devide_en = out_ready && store_en;
+  assign hash_out32 = devided32;
 
-   keccak_devide keccak_devide(
+
+  keccak_devide keccak_devide(
 			       .in512(in512),
-			       .devided32(hash_out32),
+			       .devided32(devided32),
 			       .num(hash_num),
 			       .en(devide_en)
 			       );
+
 
 endmodule
